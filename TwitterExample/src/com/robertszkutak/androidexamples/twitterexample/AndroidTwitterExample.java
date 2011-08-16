0ad95cb6b527cfffb31cdeaa822ac703c24cce88
Robert L Szkutak II <r0szsoft@gmail.com>
2011-08-16 17:23:20 -0400

    Updated the Twitter client so access tokens are stored in Android shared preferences. Users can now log in and out of Twitter multiple times

--- TwitterExample/src/com/robertszkutak/androidexamples/twitterexample/AndroidTwitterExample.java
+++ TwitterExample/src/com/robertszkutak/androidexamples/twitterexample/AndroidTwitterExample.java
@@ -28,12 +28,16 @@ import oauth.signpost.exception.OAuthMessageSignerException;
 import oauth.signpost.exception.OAuthNotAuthorizedException;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -42,7 +46,6 @@ import twitter4j.auth.AccessToken;
 
 public class AndroidTwitterExample extends Activity 
 {
-    
 	public static final String CONSUMER_KEY = "Get this from your Twitter application settings page";
 	public static final String CONSUMER_SECRET = "Get this from your Twitter application settings page";
 	
@@ -59,9 +62,11 @@ public class AndroidTwitterExample extends Activity
 	
 	private TextView loginStatus, debugStatus;
 	private EditText tweetstring;
-	private Button tweet;
+	private Button tweet, loginout;
 	
 	private static Intent newIntent = null;//Used to launch the Android web browser which is used to sign the user into Twitter and return a token if successful
+	
+	private static SharedPreferences pref = null;
 	
 	private static String debug, token, secret, authURL, uripath;
 	private static final boolean localauth = false;//Set to true if you give non-null values to ACCESS_TOKEN and ACCESS_TOKEN_SECRET and wish to authenticate without using the Android web browser
@@ -76,6 +81,8 @@ public class AndroidTwitterExample extends Activity
 	{   
 		super.onCreate(savedInstanceState);
 		
+		pref = PreferenceManager.getDefaultSharedPreferences(this);
+		
         if(localauth)
 		{
 		    token = ACCESS_TOKEN;
@@ -84,24 +91,13 @@ public class AndroidTwitterExample extends Activity
 		}
 		else
 		{	
-			try 
-			{
-				if(token == null && secret == null && auth == false && browser == false)
-					authURL = provider.retrieveRequestToken(consumer, OAUTH_CALLBACK_URL);
-				
-			} catch (OAuthMessageSignerException e) 
-			{
-				e.printStackTrace();
-			} catch (OAuthNotAuthorizedException e) 
-			{
-				e.printStackTrace();
-			} catch (OAuthExpectationFailedException e) 
-			{
-				e.printStackTrace();
-			} catch (OAuthCommunicationException e) 
-			{
-				e.printStackTrace();
-			}
+			token = pref.getString("TWITTER_OAUTH_TOKEN", "");
+			secret = pref.getString("TWITTER_OAUTH_TOKEN_SECRET", "");
+			
+			if(token != null && token != "" && secret != null && secret != "")
+				auth = true;
+			else
+				setAuthURL();
 		}
 	}
 	
@@ -111,9 +107,6 @@ public class AndroidTwitterExample extends Activity
 		
 		if(localauth == false && auth == false)
 		{	
-			
-			//TODO : Check to see if tokens are stored in preferences from previous retrieval
-			
 			if(browser == true)
 				browser2 = true;
 			
@@ -137,10 +130,13 @@ public class AndroidTwitterExample extends Activity
 				
 						token = consumer.getToken();
 						secret = consumer.getTokenSecret();
-					
-						auth = true;
 						
-						//TODO : Store tokens in preferences for future use so we don't have to reauthenticate via the Internet
+						final Editor editor = pref.edit();
+						editor.putString("TWITTER_OAUTH_TOKEN", token);
+						editor.putString("TWITTER_OAUTH_TOKEN_SECRET", secret);
+						editor.commit();
+						
+						auth = true;
 
 					} catch (OAuthMessageSignerException e) {
 						e.printStackTrace();
@@ -163,6 +159,7 @@ public class AndroidTwitterExample extends Activity
 	        tweetstring = (EditText)findViewById(R.id.tweet);
 	        debugStatus = (TextView)findViewById(R.id.debug_status);
 	        tweet = (Button) findViewById(R.id.btn_tweet);
+	        loginout = (Button) findViewById(R.id.loginout);
 			
         	debug = "Access Token: " + token + "\n\nAccess Token Secret: " + secret;
         	debugStatus.setText(debug);
@@ -171,15 +168,18 @@ public class AndroidTwitterExample extends Activity
         	{
             	public void onClick(View v)
             	{
-            		if (isAuthenticated()) 
-            		{
-            			sendTweet();
-            		} else 
-            		{
-            			//TODO Some sort of failure pop-up (eg TOAST)
-            		}
+            		sendTweet();
             	}
         	});
+        	
+        	loginout.setOnClickListener(new View.OnClickListener()
+        	{
+        		public void onClick(View v)
+        		{
+        			LogInOrOut();
+        		}
+        	});
+        	
 			updateLoginStatus();
         }
 		
@@ -187,10 +187,74 @@ public class AndroidTwitterExample extends Activity
 			finish();
 	}
 	
+	//Grabs the authorization URL from OAUTH and sets it to the String authURL member
+	public void setAuthURL()
+	{
+		try 
+		{
+			if((token == null || token == "") && (secret == null || secret == "") && auth == false && browser == false)
+				authURL = provider.retrieveRequestToken(consumer, OAUTH_CALLBACK_URL);
+			
+		} catch (OAuthMessageSignerException e) 
+		{
+			e.printStackTrace();
+		} catch (OAuthNotAuthorizedException e) 
+		{
+			e.printStackTrace();
+		} catch (OAuthExpectationFailedException e) 
+		{
+			e.printStackTrace();
+		} catch (OAuthCommunicationException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	//Logs the user in or out of Twitter depending on whether they are presently logged into Twitter
+	private void LogInOrOut()
+	{
+		if(isAuthenticated())
+			logout();
+		else
+		{
+			auth = browser = browser2 = false;
+			setAuthURL();
+			browser = true;
+			newIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(authURL));
+			startActivity(newIntent);
+		}	
+	}
+	
+	//Removes the shared preferences values and sets the current token and secret to null essentially logging the user out of Twitter
+	private void logout() 
+	{
+		final Editor edit = pref.edit();
+		edit.remove("TWITTER_OAUTH_TOKEN");
+		edit.remove("TWITTER_OAUTH_TOKEN_SECRET");
+		edit.commit();
+		
+		token = null;
+		secret = null;
+		
+		consumer = null;
+		consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+	    provider = null;
+	    provider = new CommonsHttpOAuthProvider(REQUEST_URL, ACCESS_URL, AUTHORIZE_URL);
+		
+		updateLoginStatus();
+	}
+	
 	//Updates a TextView telling us whether or not we are logged into Twitter
 	public void updateLoginStatus() 
 	{
-		loginStatus.setText("Logged into Twitter : " + isAuthenticated());
+		boolean ret = isAuthenticated();
+		
+		loginStatus.setText("Logged into Twitter : " + ret);
+		
+		if(ret == true)
+			loginout.setText("Log out of Twitter");
+		else
+			loginout.setText("Log into Twitter");
 	}
 
 	//Returns the String in an EditText. If the String is over 140 characters it is shortened to 140 characters before being returned
@@ -205,40 +269,57 @@ public class AndroidTwitterExample extends Activity
 	//Sends a Tweet without disrupting the UI
 	public void sendTweet() 
 	{
-		Thread t = new Thread() 
+		if(isAuthenticated())
 		{
-	        public void run() 
-	        {
-	        	try 
-	        	{
-	        		sendTweetToTwitter(getTweetMessage());
-				} catch (Exception ex) 
+			Thread t = new Thread() 
+			{
+				public void run() 
 				{
-					ex.printStackTrace();
+					try 
+					{
+						sendTweetToTwitter(getTweetMessage());
+					} catch (Exception ex) 
+					{
+						ex.printStackTrace();
+					}
 				}
-	        }
 
-	    };
-	    t.start();
+			};
+			t.start();
+		}
+		else
+		{
+			Toast toast = Toast.makeText(getApplicationContext(), "You are not logged into Twitter", Toast.LENGTH_SHORT);
+			toast.show();
+		}
 	}
     
 	//Checks to see if we are logged into Twitter with the correct tokens
 	public boolean isAuthenticated() 
 	{	
-		AccessToken a = new AccessToken(token,secret);
-		Twitter twitter = new TwitterFactory().getInstance();
-		twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-		twitter.setOAuthAccessToken(a);
-		
-		try 
+		if(token == null || secret == null || token == "" || secret == "")
 		{
-			twitter.getAccountSettings();
-			return true;
-		} catch (TwitterException e) 
-		{
-			debug += "\n\n" + e.getMessage();
+			debug = "";
 			debugStatus.setText(debug);
 			return false;
+		}
+		else
+		{
+			AccessToken a = new AccessToken(token,secret);
+			Twitter twitter = new TwitterFactory().getInstance();
+			twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+			twitter.setOAuthAccessToken(a);
+		
+			try 
+			{
+				twitter.getAccountSettings();
+				return true;
+			} catch (TwitterException e) 
+			{
+				debug += "\n\n" + e.getMessage();
+				debugStatus.setText(debug);
+				return false;
+			}
 		}
 	}
 	
